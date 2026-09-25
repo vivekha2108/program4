4aa2c77b4f5a040f746a5263204d924aa9dffc40
VIVEKHA E <vivekha2108@gmail.com>
2026-09-25 12:57:38 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,20 +1,15 @@
 CREATE DATABASE CollegeDB;
--Select Database
 USE CollegeDB;
--Create Course Table
 CREATE TABLE Course (
 CourseID INT PRIMARY KEY,
 CourseName VARCHAR(30) NOT NULL,
 Credits INT,
 DepartmentID INT
 );
--Insert Records
 INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
 VALUES
 (201, 'Database Systems', 4, 101),
 (202, 'Data Structures', 3, 101),
 (203, 'Computer Networks', 4, 102);
--Display Table Structure
 DESCRIBE Course;
--Display All Records
 SELECT * FROM Course;
