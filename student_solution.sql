d8fcdec3513ce71c6335bb371be84104bf9ef02e
VIVEKHA E <vivekha2108@gmail.com>
2026-09-25 12:54:45 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,22 +1,20 @@
--- =========================================
--- SQL Assignment: Create Course Table
--- Name:
--- Register Number:
--- =========================================
-
--- Create a table named Course with:
--- CourseID
--- CourseName
--- Credits
--- DepartmentID
-
--- Add CourseID as PRIMARY KEY.
-
-
--- Insert at least 3 records.
-
-
--- Display the table structure using DESCRIBE.
-
-
--- Display all records.
+CREATE DATABASE CollegeDB;
+-Select Database
+USE CollegeDB;
+-Create Course Table
+CREATE TABLE Course (
+CourseID INT PRIMARY KEY,
+CourseName VARCHAR(30) NOT NULL,
+Credits INT,
+DepartmentID INT
+);
+-Insert Records
+INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
+VALUES
+(201, 'Database Systems', 4, 101),
+(202, 'Data Structures', 3, 101),
+(203, 'Computer Networks', 4, 102);
+-Display Table Structure
+DESCRIBE Course;
+-Display All Records
+SELECT * FROM Course;
